508588f0335a1529092f5172edd2fe22b019a50b
Kripesh Sharma <sharma.kripesh22@gmail.com>
2021-10-27 10:51:03 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+// Create variables for the game state
+let player1Score = 0
+let player2Score = 0
+let player1Turn = true
+
+// Create variables to store references to the necessary DOM nodes
+const player1Dice = document.getElementById("player1Dice")
+const player2Dice = document.getElementById("player2Dice")
+const player1Scoreboard = document.getElementById("player1Scoreboard")
+const player2Scoreboard = document.getElementById("player2Scoreboard")
+const message = document.getElementById("message")
+const rollBtn = document.getElementById("rollBtn")
+const resetBtn = document.getElementById("resetBtn")
+
+function showResetButton() {
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+}
+
+function showRollDiceButton() {
+    rollBtn.style.display = "block"
+    resetBtn.style.display = "none"
+}
+
+function reset() {
+    player1Dice.textContent = "-"
+    player2Dice.textContent = "-"
+    message.textContent = "Player 1 turn"
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Score = 0
+    player2Score = 0
+    player1Turn = true
+    showRollDiceButton()
+    
+}
+
+rollBtn.addEventListener("click", function() {
+     const randomNumber = Math.floor(Math.random() * 6) + 1
+     
+     if (player1Turn) {
+         player1Dice.textContent = randomNumber
+         player1Score += randomNumber
+         player1Scoreboard.textContent = player1Score
+         message.textContent = "Player 1 Turn"
+         player1Dice.classList.add("active")
+         player2Dice.classList.remove("active")
+     } else{
+         player2Dice.textContent = randomNumber
+         player2Score += randomNumber
+         player2Scoreboard.textContent = player2Score
+         message.textContent = "Player 2 Turn"
+         player2Dice.classList.add("active")
+         player1Dice.classList.remove("active")
+     }
+     
+      if (player1Score >= 20) {
+        message.textContent = "Player 1 has won! 🥳"
+        showResetButton()
+     } else if (player2Score >= 20) {
+        message.textContent = "Player 2 has won! 🎉"
+        showResetButton()
+     }
+
+     player1Turn = !player1Turn
+ })
+ 
+
+
+resetBtn.addEventListener("click" , function() {
+         reset()
+     }) 
+
+ 
+ 
